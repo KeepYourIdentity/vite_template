0e9536b999cebc11ab6a92f6ee5ca2e70b30ce22
Spring Bonnie <zidaneachmad077@gmail.com>
2026-07-22 18:38:10 +0700

    chore(checkpoint): unfinnish work, continue later

--- src/features/ui/components/layout/Navbar/useIndex.ts
+++ src/features/ui/components/layout/Navbar/useIndex.ts
@@ -1,13 +1,13 @@
 import { useCallback } from "react";
 import { useNavigate } from "react-router-dom";
-import { useSidebarStore } from "src/components/store";
+import { useSidebarControll } from "core/store";
 import { logout } from "src/utils";
 
 export const useNavbar = () => {
   const navigate = useNavigate();
-  const toggleSidebar = useSidebarStore((state) => state.toggleSidebar);
-  const haveSidebar = useSidebarStore((state) => state.haveSidebar);
-  const setHaveSidebar = useSidebarStore((state) => state.setHaveSidebar);
+  const toggleSidebar = useSidebarControll((state) => state.toggleSidebar);
+  const haveSidebar = useSidebarControll((state) => state.haveSidebar);
+  const setHaveSidebar = useSidebarControll((state) => state.setHaveSidebar);
 
   const handleToggle = useCallback(() => {
     if (haveSidebar) {
